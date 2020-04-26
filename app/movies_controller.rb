2bb4d9a2f28af80437e7e776a026b2a7a4c4cc22
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-26 17:24:24 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_in_a_block(title: "Home Alone", release_date: 1990)
   # release_date == 1990
   
   Movie.create do |m|
-    m = "Home Alone"
+    m.title  = "Home Alone"
     m.release_date = 1990
   end
 end
