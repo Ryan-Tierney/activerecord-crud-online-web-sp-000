cc78efb755212b5c62887f992702eae37c0a89a7
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-04-26 17:02:07 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -35,7 +35,8 @@ def can_be_created_in_a_block(title: "The Room", release_date: 2003)
 end
 
 def can_get_the_first_item_in_the_database
-   Movie.title.first
+  movie = Movie.title
+  movie.first 
 end
 
 def can_get_the_last_item_in_the_database
